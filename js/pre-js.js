0a45fea3e9b3fab35e7809c16d5b413354a33152
Jose F. Morales <jfmcjf@gmail.com>
2023-05-14 11:44:27 +0200

    (ciaowasm) disable emscripten image/audio decoding (suppress warning)
    Src-commit: 4510745f7c28744cd1b06494aa32facb8bb7aa63

--- js/pre-js.js
+++ js/pre-js.js
@@ -7,4 +7,6 @@ var CIAOENGINE = {
     Module['preRun'].push(function() { Module['FS'] = FS; });
     // Make ENV visible
     if (!Module['getENV']) Module['getENV'] = function() { return ENV; };
-
+    // Disable image and audio decoding
+    Module.noImageDecoding = true;
+    Module.noAudioDecoding = true;
